78be892e652597318ae4f62b876f01b612cc8a4e
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-08-27 21:25:08 -0300

    revendo as aulas - comentando partes importantes

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const personagensCont = document.getElementById('personagens');
-const luasCont = document.getElementById('luas');
-const planetasCont = document.getElementById('planetas');
-const navesCont = document.getElementById('naves');
-
-preencherContadores();
+const personagensCont = document.getElementById("personagens");
+const luasCont = document.getElementById("luas");
+const planetasCont = document.getElementById("planetas");
+const navesCont = document.getElementById("naves");
 
 function preencherContadores() {
-    Promise.all([swapGet('people/'),
-                 swapGet('vehicles/'),
-                 swapGet('planets/'),
-                 swapGet('starships/')])
-        .then(function (results) {
-            console.log(results);
-            personagensCont.innerHTML = results[0].data.count;
-            luasCont.innerHTML = results[1].data.count;
-            planetasCont.innerHTML = results[2].data.count;
-            navesCont.innerHTML = results[3].data.count;
-        });
+  // Ele espera pegar todos os dados com a Promise, para só depois atualizar na tela, quando todos os valores já tiverem retornado
+  Promise.all([
+    swapGet("people/"),
+    swapGet("vehicles/"),
+    swapGet("planets/"),
+    swapGet("starships/"),
+  ]).then(function (results) {
+    console.log(results);
+    personagensCont.innerHTML = results[0].data.count;
+    luasCont.innerHTML = results[1].data.count;
+    planetasCont.innerHTML = results[2].data.count;
+    navesCont.innerHTML = results[3].data.count;
+  });
 }
 
 function swapGet(param) {
-    return axios.get(`https://swapi.dev/api/${param}`)
+  return axios.get(`https://swapi.dev/api/${param}`);
 }
+
+preencherContadores();
